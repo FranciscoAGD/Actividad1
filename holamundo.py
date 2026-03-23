f12aeb81c3b3489df18e036c0173d40f23738fb1
Daniel Castillejos <al07099742@tecmilenio.mx>
2026-03-23 16:37:32 -0700

    Cambios de merge

--- holamundo.py
+++ holamundo.py
@@ -1,15 +1,5 @@
 print("hola mundo")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-def mostrar_nombre():
-    print("Daniel Castillejos")
->>>>>>> 956c1eb (Correciones)
-
-
-<<<<<<< HEAD
 def Francisco():
     print("Francisco")
 
@@ -17,19 +7,13 @@ def Francisco():
 if __name__ == '__main__':
     Francisco()
 
-=======
-
-
->>>>>>> 5b792aaafda559548c25efc88b59f5cbfdd4a139
 def mostrar_nombre():
     print("Daniel Castillejos")
 
 mostrar_nombre()
 
 
-<<<<<<< HEAD
+def mostrar_nombre():
+    print("Diego Aramburo")
 
-=======
->>>>>>> 1e5e305 (Cambios en el codigo de la funcion daniel)
-=======
->>>>>>> 5b792aaafda559548c25efc88b59f5cbfdd4a139
+mostrar_nombre()
