21cb2c637fd83b320f7274a958a72fbe4cfcc232
Daniel Castillejos <al07099742@tecmilenio.mx>
2026-03-23 11:41:01 -0700

    Creacion de funcion, muestra mi nombre en el archivo holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -1,8 +1,15 @@
 print("hola mundo")
 
+
 def Francisco():
     print("Francisco")
 
 
 if __name__ == '__main__':
     Francisco()
+
+def mostrar_nombre():
+    print("Daniel Castillejos")
+
+mostrar_nombre()
+
